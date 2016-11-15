590fe85c465e21f9aba9558741e3dbaabb06c714
Etienne Bruines <etienne@bruines.com>
2016-11-15 09:45:30 +0100

    Initial symfony package

---
+++ README.md
@@ -0,0 +1,4 @@
+sollicitatie
+============
+
+A Symfony project created on November 15, 2016, 9:23 am.
---
+++ index.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<body>
+<h1>PHP Version <?php echo phpversion(); ?></h1>
+<p>Please run <code>caddyphp</code> in the <code>web</code> directory. </p>
+</body>
+</html>
